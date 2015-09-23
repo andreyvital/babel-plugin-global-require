8b73cd0f2721274fbacc05a81c83bfd1361fe9fb
Andrey <andreykvital@gmail.com>
2015-09-23 09:31:26 -0300

    Add generateGlobalMap.js

---
+++ generateGlobalMap.js
@@ -0,0 +1,36 @@
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function generateGlobalMap(root) {
+  return fetchAllFiles(root).map(function(file) {
+    return {
+      path: file,
+      name: path.basename(file, path.extname(file))
+    }
+  })
+}
+
+/**
+ * @param   {String} root
+ * @returns {String[]}
+ */
+function fetchAllFiles(root) {
+  var results = []
+
+  fs.readdirSync(root).forEach(function(file) {
+    var absolute = path.join(root, file)
+    var stat = fs.statSync(absolute)
+
+    if (stat.isDirectory()) {
+      results = results.concat(fetchAllFiles(absolute))
+      return
+    }
+
+    results.push(absolute)
+  })
+
+  return results
+}
