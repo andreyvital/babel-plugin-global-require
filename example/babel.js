9b8f329973909443b4c17cd14c0136fe5b590310
Andrey <andreykvital@gmail.com>
2015-09-23 13:57:11 -0300

    Fix example

--- example/babel.js
+++ example/babel.js
@@ -1,10 +1,6 @@
 require('babel/register')({
   plugins: [
-    require('../global-require.js')({
-      root: __dirname + '/src',
-      exclude: /node_modules/,
-      node_modules: __dirname + '/node_modules'
-    })
+    require('../global-require.js')
   ]
 })
 
