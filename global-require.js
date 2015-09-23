5a0c39134fdf450c424468024956952bd84ca380
Andrey <andreykvital@gmail.com>
2015-09-23 13:56:24 -0300

    Update package.json

--- global-require.js
+++ global-require.js
@@ -1,59 +1,65 @@
 /**
  * @author Andrey K. Vital <andreykvital@gmail.com>
  */
+var fs = require('fs')
+var path = require('path')
 var generateGlobalMap = require('./generateGlobalMap.js')
 
-module.exports = function(opts) {
+var opts = {}
+var config = path.resolve('.global-require')
+
+if (fs.existsSync(config)) {
+  opts = JSON.parse(fs.readFileSync(config), function(key, value) {
+    return key === 'exclude' ? new RegExp(value) : value
+  })
+}
+
+module.exports = function(babel) {
+  var t = babel.types
   var globalMap = {}
 
   if (opts.root) {
     globalMap = generateGlobalMap(
-      opts.root,
-      opts.node_modules,
+      path.resolve(opts.root),
+      path.resolve(opts.node_modules),
       opts.exclude
     )
   }
 
-  return function(babel) {
-    var t = babel.types
+  return new babel.Plugin('babel-plugin-global-require', {
+    visitor: {
+      ImportDeclaration: function(node, parent) {
+        var what = node.source.value
 
-    return new babel.Plugin('babel-plugin-global-require', {
-      visitor: {
-        ImportDeclaration: function(node, parent) {
-          var what = node.source.value
+        if (globalMap[what]) {
+          node.source.value = globalMap[what].path
+        }
 
-          if (globalMap[what]) {
-            node.source.value = globalMap[what].path
-          }
+        return node
+      },
+      CallExpression: function(node, parent) {
+        var isRequireCall = t.isIdentifier(node.callee, {
+          name: 'require'
+        })
 
-          return node
-        },
-        CallExpression: function(node, parent) {
-          if (! isRequireCall(t, node)) {
-            return
-          }
-
-          if (node.arguments[0] && t.isLiteral(node.arguments[0])) {
-            var what = node.arguments[0].value
-
-            if (! globalMap[what]) {
-              return node
-            }
-
-            return t.callExpression(node.callee, [
-              t.literal(globalMap[what].path)
-            ])
-          }
-
+        if (! isRequireCall) {
           return node
         }
-      }
-    })
-  }
-}
 
-function isRequireCall(t, call) {
-  return t.isIdentifier(call.callee, {
-    name: 'require'
+        if (node.arguments[0] && t.isLiteral(node.arguments[0])) {
+          var what = node.arguments[0].value
+
+          if (! globalMap[what]) {
+            return node
+          }
+
+          return t.callExpression(node.callee, [
+            t.literal(globalMap[what].path)
+          ])
+        }
+
+        return node
+      }
+    }
   })
 }
