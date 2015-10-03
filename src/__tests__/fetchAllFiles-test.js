48d9f8311aed02edacfe27943e7f67035ef3b4ed
Andrey <andreykvital@gmail.com>
2015-10-03 16:46:06 -0300

    Add fetchAllFiles-test

---
+++ src/__tests__/fetchAllFiles-test.js
@@ -0,0 +1,10 @@
+/**
+ * @author Andrey K. Vital <andreykvital@gmail.com>
+ */
+var expect = require('chai').expect
+var fetchAllFiles = require('../fetchAllFiles')
+
+describe('fetchAllFiles', () => {
+  it('scans directory recursively')
+  it('respects the exclude expression')
+})
